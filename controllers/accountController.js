faae43cedbf312b52c669c6a4a26cfdd2d039eab
ClarabelleCheng-Yue <cachengyue@gmail.com>
2016-09-15 10:40:25 -0700

    Add error catch

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -6,6 +6,13 @@ module.exports = {
   deleteAcct: function(req, res, next) {
     var username = req.body.username;
     //var token = req.headers['x-access-token'];
+    //delete all entries of user
+    db.Entries.destroy({ where: {username: username}}).then(function(result) {
+      res.status(200).send('Successfully deleted entries of ' + username);
+    }).catch(function(err) {
+      console.log(err, 'error');
+      res.status(500).send('Error in deleting entries');
+    });
 
     db.User.destroy({ where: {username: username} }).then(function(result) {
       res.status(200).send('Successfully deleted');

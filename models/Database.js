5177ec6fdd4edbf027a8445c477505a717605d87
p-herbert <pete.a.herbert@gmail.com>
2016-09-14 19:23:31 -0700

    Create the Comments table

--- models/Database.js
+++ models/Database.js
@@ -3,7 +3,7 @@ var connectionString;
 if(process.env.DATABASE_URL){
   connectionString = process.env.DATABASE_URL;
 } else {
-  var connectionString = require('../config/db.js'); 
+  var connectionString = require('../config/db.js');
 }
 
 var sequelize = new Sequelize(connectionString, {
@@ -27,6 +27,12 @@ var Entry = sequelize.define('entry', {
   }
 });
 
+// Define the model that corresponds to the comment table in the database.
+var Comments = sequelize.define('comments', {
+  text: Sequelize.STRING,
+  location: Sequelize.STRING
+});
+
 var Relationships = sequelize.define('relationships', {
   user1: Sequelize.INTEGER,
   user2: Sequelize.INTEGER
@@ -47,7 +53,7 @@ var Request = sequelize.define('request', {
           } else {
             next();
           }
-        })
+        });
     },
     mustNotBeDuplicateRequest: function(next) {
       Request.findOne({ where: {
@@ -61,28 +67,35 @@ var Request = sequelize.define('request', {
           } else {
             next();
           }
-        })
+        });
     }
   }
-})
+});
 
 // puts a UserId column on each Entry instance
 // also gives us the `.setUser` method available
 // after creating a new instance of Entry
-Entry.belongsTo(User)
-Request.belongsTo(User)
+Entry.belongsTo(User);
+Request.belongsTo(User);
+
+Comments.belongsTo(User);
+Comments.belongsTo(Entry);
 
 User.hasMany(Entry);
 User.hasMany(Request);
 
+User.hasMany(Comments);
+Entry.hasMany(Comments);
 
 User.sync();
 Entry.sync();
 Relationships.sync();
-Request.sync()
+Request.sync();
+Comments.sync();
 
 module.exports.User = User;
 
 module.exports.Entry = Entry;
 module.exports.Relationships = Relationships;
 module.exports.Request = Request;
+module.exports.Comments = Comments;

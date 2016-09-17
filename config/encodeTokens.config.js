04913550e78aa840fa1b7c5db0ba62cfdc28314d
oldchevy <nick.olszowy@gmail.com>
2016-09-17 14:40:00 -0700

    Fixing bugs

--- config/encodeTokens.config.js
+++
@@ -1,3 +0,0 @@
-module.exports.tokenKey = '';
-module.exports.phoneNumberKey = '';
-//Insert your keys to decode the db here;
--- controllers/accountController.js
+++ controllers/accountController.js
@@ -6,7 +6,7 @@ module.exports = {
   deleteAcct: function(req, res, next) {
     var username = req.body.username;
     db.User.destroy({ where: {username: username} }).then(function(result) {
-      res.status(200).send('Successfully deleted');
+      res.status(200).send();
     }).catch(function(err) {
       console.log(err, 'error');
       res.status(500).send('Error in deletion');
